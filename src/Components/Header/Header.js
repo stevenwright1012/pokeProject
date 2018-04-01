5a3a461f03f418f7c5fb79b5c3bfd5277bece86d
Steven <stevenwright1012@gmail.com>
2018-04-01 14:02:02 -0600

    need to add PokeBox component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -8,47 +8,56 @@ class Header extends Component{
         super()
 
         this.state = {
-            winner: '',
-            loser: '',
+            pokePhraseLeft: '',
+            pokePhraseRight: '',
         }
         this.battle = this.battle.bind(this)
         this.reset = this.reset.bind(this)
     }
 
     battle(){
+        if(this.props.pokemon1Full === false || this.props.pokemon2Full === false){
+            alert("You have not selected a Pokemon. Click on a Pokemon's picture to get started")
+        }
+        else{
         let pokemon1 = this.props.pokemon1;
         let pokemon2 = this.props.pokemon2;
         let postBattle1 = (pokemon1.stats[5].base_stat -pokemon2.stats[4].base_stat) + pokemon1.stats[3].base_stat;
         let postBattle2 = (pokemon2.stats[5].base_stat - pokemon1.stats[4].base_stat) + pokemon1.stats[3].base_stat;
         if (postBattle1 > postBattle2){
             this.setState({
-                winner: `${pokemon1.name} is the winner`,
-                loser: `${pokemon2.name} is the loser`
+                pokePhraseLeft: `${pokemon1.name} is the winner`,
+                pokePhraseRight: `${pokemon2.name} is the loser`
             })
         }
         else if(postBattle1 < postBattle2){
             this.setState({
-                winner:`${pokemon2.name} is the winner`,
-                loser: `${pokemon1.name} is the loser`,
+                pokePhraseLeft:`${pokemon1.name} is the loser`,
+                pokePhraseRight: `${pokemon2.name} is the winner`,
             })
         }else if(postBattle2 === postBattle1){
             this.setState({
-                winner: "It's a tie",
-                loser: "It's a tie"
+                pokePhraseLeft: "It's a tie",
+                pokePhraseRight: "It's a tie"
             })
         }
     }
+    }
     reset(){
         this.setState({
-            winner:"",
-            loser: "",
+            pokePhraseLeft:"",
+            pokePhraseRight: "",
         })
         this.props.resetFn();
     }
     render(){
         return(
             <div className="top">
-                <div>{this.state.winner}</div>
+                <div className="poke_phrase">
+                {
+                <h1>{this.state.pokePhraseLeft}</h1>
+                }
+                </div>
                 <button className="battle_button" onClick={this.battle}>
                     Battle!
                 </button>
@@ -60,7 +69,6 @@ class Header extends Component{
                     <img className="batte_image" src={`./pokesprites/${this.props.pokemon1.id}.png`} alt="pokemon"/>
                     :
                     null
-                    // <img className="batte_image" src="https://sixprizes.com/wp-content/uploads/2014/05/poke-ball-open-beginning-1.jpg" alt="pokeball"/> 
                 }
                 </div>
                 {
@@ -68,7 +76,7 @@ class Header extends Component{
                     ?
                     <Spinner />
                     :
-                    null
+                    <h1>VS</h1>
                 }
                 <div className="pokemon">
                 {this.props.pokemon2.name}
@@ -78,10 +86,11 @@ class Header extends Component{
                     <img className="batte_image" src={`./pokesprites/${this.props.pokemon2.id}.png`} alt="pokemon"/>
                     :
                     null
-                    // <img className="batte_image" src="https://sixprizes.com/wp-content/uploads/2014/05/poke-ball-open-beginning-1.jpg" alt="pokeball"/> 
                 }
                 </div>
-                <div>{this.state.loser}</div>
+                <div className="poke_phrase">
+                    <h1>{this.state.pokePhraseRight}</h1>
+                </div>
                 <button className='reset_button'onClick={this.reset}>Reset</button>
             </div>
         )
---
+++ src/Components/Header/PokeBox/PokeBox.js
@@ -0,0 +1,13 @@
+// import React, {Component} from 'react';
+
+// class PokeBox extends Component{
+//     render(){
+//         return (
+//             <div>
+
+//             </div>
+//         )
+//     }
+// }
+
+// export default PokeBox;
